dc80c60bd381ac5e51b027bed0e18e7ef5a3bd7e
Unmesh Patil <punmesh56@gmail.com>
2026-03-04 09:14:39 +0530

    Add files via upload

---
+++ ViteReact/Footer.jsx
@@ -0,0 +1,12 @@
+// Use rafce to install all things 
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      &copy; copyright Unmesh Patil | All Rights Reserved
+    </div>
+  )
+}
+
+export default Footer
---
+++ ViteReact/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Navbar.css"
+
+const navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contacat</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
