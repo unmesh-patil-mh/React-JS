0349896337dde455b8ed2cbde0836f3ad3f9b8a3
Unmesh Patil <punmesh56@gmail.com>
2026-04-01 11:47:23 +0530

    Add files via upload

---
+++ UseContextHook/WithoutContextAPI/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+const Navbar = ({count}) => {
+  return (
+    <div>
+      Navbar
+      <Button count={count}/>
+    </div>
+  )
+}
+
+export default Navbar
